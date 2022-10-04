18ad138bc4b603ef3e8b381012d51e8498b3d2b0
dearpriyankasa <dearpriyankasa@gmail.com>
2022-10-04 18:41:41 -0400

    Added comments to the state handler i.e. the data flow files

--- frontend/src/features/GetIngredients/getIngredients.saga.ts
+++ frontend/src/features/GetIngredients/getIngredients.saga.ts
@@ -1,3 +1,9 @@
+/**
+ * File name: getIngredients.saga.ts
+ * Task - It is a middleware, that handles the async logic in the data flow based on the action that 
+ * has been dispatched
+ * @author Priyanka Ambawane - dearpriyankasa@gmail.com
+ */
 import { takeEvery, put, call } from 'redux-saga/effects';
 import * as ACTIONS from './getIngredients.action';
 import * as ACTION_TYPES from './getIngredients.actionTypes';
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,15 +1,19 @@
 /**
- * Configure Store
- * @author dearpriyankasa@gmail.com
+ * File name: store.ts
+ * Task - This file configures the enire application's store using the redux state library
+ * It initializes and combines all the reducers and the async middleware functionalities for API
+ * @author Priyanka Ambawane - dearpriyankasa@gmail.com
  */
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import combineReducer from './features/features.reducer';
 import combinedSagas from './features/features.saga';
  
+// initializes async functionality for the API calls
 const sagaMiddleware = createSagaMiddleware();
  
 export default function applicationStore() {
+   // create the application store
    const store = createStore(combineReducer, applyMiddleware(sagaMiddleware));
    sagaMiddleware.run(combinedSagas);
    return store;

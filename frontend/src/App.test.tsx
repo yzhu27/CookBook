a903ba2fa2921f6d4972b054b6606c424d4c1c9a
Karan Suresh Kajani <karan.kajani98@gmail.com>
2022-10-06 16:43:06 -0400

    Added Tests for App.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { getByTestId, getByDisplayValue, getByLabelText, getByTitle, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/*test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});*/
+
+test('has header component', () => {
+  const {getByTestId} = render(<App />);
+  expect(getByTestId("header-comp-43")).toBeInTheDocument();
+});
+
+test('has search component', () => {
+  const {getByTestId} = render(<App />);
+  expect(getByTestId("search-comp-43")).toBeInTheDocument();
+});
+
+test('has body component', () => {
+  const {getByTestId} = render(<App />);
+  expect(getByTestId("body-comp-43")).toBeInTheDocument();
+});
+
+test('shows search bar correctly', () => {
+  const {getByLabelText} = render(<App />);
+  expect(getByLabelText("Type to select Ingredients")).toBeInTheDocument();
 });

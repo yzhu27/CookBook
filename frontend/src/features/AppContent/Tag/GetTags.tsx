d4e6e520b84ee8f05453e93e53b6457762f9159e
Pinxiang Wang <pwang25@ncsu.edu>
2022-12-02 09:49:40 -0500

    add tags

--- frontend/src/features/AppContent/Tag/GetTags.tsx
+++ frontend/src/features/AppContent/Tag/GetTags.tsx
@@ -19,88 +19,45 @@ this file. If not, please write to: help.cookbook@gmail.com
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import {Box, Button, Chip, Grid, TextField} from '@mui/material'
+import { Box, Button, Chip, Grid, TextField } from '@mui/material'
 import { useForm, Controller } from 'react-hook-form'
 import Send from '@mui/icons-material/Send'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import { getTagListInitiator } from './getTagList.action'
 import getTagListAppState from "./getTags.reducer";
-import {getRecipeListInitiator} from "../RecipeList/getRecipeList.action";
-
+import { getRecipeListInitiator } from "../RecipeList/getRecipeList.action";
 
 interface ChipData {
   key: string
   label: string
 }
-interface ListData {
-  key: string
-  label: string
-}
-const Tags = ({
-  label,
-  id,
-} : any) =>{
-  return (
-       <TextField
-            id="test"
-            label="Standard"
-            variant = "standard"
-       />
-  )
-}
-const GetTags= () => {
+const GetTags = () => {
   const dispatch = useDispatch()
   const navigateTo = useNavigate()
-
   const [chipData, setChipData] = useState<readonly ChipData[]>([])
-  const [listData, setListData] = useState<readonly ListData[]>([])
-
-  // accesses the state of the component from the app's store
-  const getTagsState = useSelector(
-    (state: any) => state.getTagListAppState
-  )
-  useEffect(() => {
-    let tags = getTagsState.getTagListData
-    if (Array.isArray(tags)) {
-      tags.forEach((item: string, index: number) => {
-        setListData((list) => list.concat({ key: item, label: item }))
-      })
+  const reciptList = ['milk', 'butter', 'blueberries','grapefruits','wine vinegar']
+  const getReciptButton = (name: string, key: number) => {
+    const onSubmit = () => {
+      let ingredientsArray: Array<string> = []
+      ingredientsArray.push(name.toLocaleLowerCase())
+      if (ingredientsArray.length > 0) {
+        sessionStorage.setItem('ingredients', JSON.stringify(ingredientsArray))
+        dispatch(
+          getRecipeListInitiator('http://localhost:8000/recipe/search/', {
+            ingredients: ingredientsArray,
+            page: 1,
+          })
+        )
+        navigateTo('/recipe-list')
+      }
     }
-    return () => {
-      setListData([])
-    }
-  }, [getTagsState.getTagListData])
 
-  // on enter or ingredient selection from suggestion list, this function stores the input in the chipData state
-  // const onChangeField = (val: string) => {
-  //   setChipData((chips) => chips.concat({ key: val, label: val }))
-  // }
-  const requestTags = () =>{
-    // fetch("https://api.example.com/items")
-    //   .then(res => res.json())
-    //   .then(
-    //     (results) => {
-    //       results.forEach(
-    //             setChipData(
-    //                 (chips) => chips.concat({ key: results, label: results}))
-    //       )
-    //     },
-    //     // Note: it's important to handle errors here
-    //     // instead of a catch() block so that we don't swallow
-    //     // exceptions from actual bugs in components.
-    //     (error) => {
-    //
-    //     }
-    //   )
-    setChipData((chips) => chips.concat({ key: 'aa', label: 'aa'}))
+    return <Button
+      key={key}
+      onClick={onSubmit}
+      type="submit"
+      variant="contained"> {name} </Button>
   }
-
-  // handler to delete the ingredient
-  // const handleDelete = (chipToDelete: ChipData) => () => {
-  //   setChipData((chips) =>
-  //     chips.filter((chip) => chip.key !== chipToDelete.key)
-  //   )
-  // }
 
   // handler to trigger the API call to get the list of recipes according to the user's ingredient's input
   const onSubmit1 = () => {
@@ -117,34 +74,7 @@ const GetTags= () => {
       navigateTo('/recipe-list')
     }
   }
-   const onSubmit2 = () => {
-    let ingredientsArray: Array<string> = []
-    ingredientsArray.push('milk'.toLocaleLowerCase())
-    if (ingredientsArray.length > 0) {
-      sessionStorage.setItem('ingredients', JSON.stringify(ingredientsArray))
-      dispatch(
-        getRecipeListInitiator('http://localhost:8000/recipe/search/', {
-          ingredients: ingredientsArray,
-          page: 1,
-        })
-      )
-      navigateTo('/recipe-list')
-    }
-  }
-   const onSubmit3 = () => {
-    let ingredientsArray: Array<string> = []
-    ingredientsArray.push('blueberries'.toLocaleLowerCase())
-    if (ingredientsArray.length > 0) {
-      sessionStorage.setItem('ingredients', JSON.stringify(ingredientsArray))
-      dispatch(
-        getRecipeListInitiator('http://localhost:8000/recipe/search/', {
-          ingredients: ingredientsArray,
-          page: 1,
-        })
-      )
-      navigateTo('/recipe-list')
-    }
-  }
+
   return (
     <>
       <Box
@@ -152,33 +82,22 @@ const GetTags= () => {
         display="flex"
         justifyContent="center"
         alignItems="center">
-          <Grid container spacing={2}>
-              <Grid item xs={12}>
-          <Box paddingTop={'20px'}>
-            {chipData.map((data) => {
-              return (
-                <Button
-                  onClick = {onSubmit1}
-                  type="submit"
-                  variant="contained"
-                ></Button>
-              )
-            })}
-          </Box>
-        </Grid>
-                   <Button
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Box paddingTop={'20px'}>
+              {chipData.map((data) => {
+                return (
+                  <Button
                     onClick={onSubmit1}
                     type="submit"
-                    variant="contained"> butter </Button>
-                   <Button
-                    onClick={onSubmit3}
-                    type="submit"
-                    variant="contained"> blueberries </Button>
-                   <Button
-                    onClick={onSubmit2}
-                    type="submit"
-                    variant="contained"> milk </Button>
+                    variant="contained"
+                  ></Button>
+                )
+              })}
+            </Box>
           </Grid>
+          {reciptList.map((v, i) => getReciptButton(v, i))}
+        </Grid>
       </Box>
     </>
   )
